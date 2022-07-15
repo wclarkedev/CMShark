991ec5d5795aad8ca8fe2a7fb78c4ce8632ebb00
Will Clarke <clarke19w@gmail.com>
2022-07-15 23:32:43 +0100

    styling

--- public/admin/index.php
+++ public/admin/index.php
@@ -13,10 +13,17 @@ require '../../config/config.php';
     <body class="admin-body">
         <div class="form-container">
             <form method="POST" class="form">
+                <h2 class="heading">Edit page</h2>
                 <section class="form-section">
                     <h3>Page Header</h3>
                     <label>Display Name</label>
-                    <input class="text-input">
+                    <input class="text-input" type="text" name="userDisplayName" placeholder="John Doe" maxlength="45">
+                    <label>Pronouns</label>
+                    <input class="text-input" type="text" name="userPronouns" placeholder="He/Him" maxlength="45">
+                    <span class="disclaimer">Read more about pronouns <a href="https://www.edi.nih.gov/blog/communities/what-are-gender-pronouns-why-do-they-matter" target="_blank">here</a>.</span>
+                    <label>Picture</label>
+                    <input type="file" value="Upload Image" name="userProfilePicture"> <!-- https://www.w3schools.com/php/php_file_upload.asp -->
+                    <input type="submit" class="submit-button" value="Save">
                 </section>
                 <section class="form-section">
                     <h3>Page Appearance</h3>
