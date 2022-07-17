265f8c642c5f2d51f10dd5ccfab97b95f328a8d9
Will Clarke <clarke19w@gmail.com>
2022-07-17 23:48:01 +0100

    db connect + disconnect

--- config/db.php
+++ config/db.php
@@ -4,6 +4,20 @@ define('DB_USER','');
 define('DB_PASSWORD','');
 define('DB_NAME','');
 
-function dbConnect(){}
-function dbDisconnect(){}
+function dbConnect(){
+    $host = DB_HOST;
+    $user = DB_USER;
+    $password = DB_PASSWORD;
+    $name = DB_NAME;
+    try {
+        $connection = new PDO("mysql:host=$host;dbname=$name",$user,$password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return 'Connected';
+    } catch (PDOException $e) {
+        return 'Connection failed - '.$e->getMessage();
+    }
+}
+function dbDisconnect($connection){
+    $connection = null;
+}
 function dbQuery(){}
