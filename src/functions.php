6019c27a060f21e04cfecaedf9c879767dc4724e
Will Clarke <clarke19w@gmail.com>
2022-07-17 23:22:29 +0100

    validation functions

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,18 @@ function redirectForNotLoggedIn(){
         header("Location: ../login/");
     }
 }
-function validateEmail($email){}
-function validatePassword($password){}
-function validateUsername($username){}
-function validateInput($input,$type){}
+function validateEmail($badEmail){
+    $email = filter_var($badEmail, FILTER_SANITIZE_EMAIL);
+    if (empty(trim($email))) return false;
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+function validatePassword($password) {
+    if (empty(trim($password))) return false;
+    if (strlen($password) <= 8 ) return 'Password must be at least 8 characters long.';
+    if (!preg_match("#[a-z]+#",$password)) return 'Password must contain 1 lower-case letter.';
+    if (!preg_match("#[A-Z]+#",$password)) return 'Password must contain 1 upper-case letter.';
+    if (!preg_match("#[0-9]+#",$password)) return 'Password must contain 1 number.';
+    if (!preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$password)) return 'Password must contain 1 spacial character';   
+}
+/*function validateUsername($username){}
+function validateInput($input,$type){}*/
