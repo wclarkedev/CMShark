18e6c398e456327a8b2c282170b5d2135de0e124
Will Clarke <clarke19w@gmail.com>
2022-07-23 14:32:51 +0100

    comments

--- public/index.php
+++ public/index.php
@@ -4,11 +4,21 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title><?php //echo getMetaTitle();?></title>
         <link href="../src/css/framework.css" rel="stylesheet">
+        <meta property="og:type" content="website">
+
+        <!-- SEO based meta info: -->
     </head>
     <body class="main-body">
-        <section class="header-container"></section>
-        <section class="body-container"></section>
+        <section class="header-container">
+            <!-- Shows the header based on user configuration in settings -->
+        </section>
+        <section class="body-container">
+            <div class="link-collection">
+                <!-- Has all link code inside and links should be displayed -->
+            </div>
+        </section>
         <section class="footer-container">
+            <!-- Shows Privacy policy -->
             <?php
             if (true/*userGiveCredit()*/){
                 ?> 
