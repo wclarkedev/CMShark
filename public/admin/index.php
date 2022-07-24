a960b764dce7dcdad4d8f807894de464182b43b1
Will Clarke <clarke19w@gmail.com>
2022-07-24 21:53:59 +0100

    remove less from main styling stack, add tw

--- public/admin/index.php
+++ public/admin/index.php
@@ -6,20 +6,20 @@ require '../../src/functions.php';
 <!doctype html><html lang="en">
     <head>
         <meta name="viewport" content="width=device-width,intial-scale=1.0">
-        <link href="../../src/css/framework.css" rel="stylesheet">
+        <link href="../../src/css/tw-output.css" rel="stylesheet">
     </head>
     <body class="shark-admin">
-        <nav class="shark-nav-bar">
+        <nav class="">
             <ul>
-                <li><a href="#" class="nav-link nav-active-item">Home</a></li>
-                <li><a href="page-settings.php?ref=dash" class="nav-link">Page Settings</a></li>
-                <li><a href="admin-settings.php?ref=dash" class="nav-link">Admin Settings</a></li>
+                <li><a href="#" class="">Home</a></li>
+                <li><a href="page-settings.php?ref=dash" class="">Page Settings</a></li>
+                <li><a href="admin-settings.php?ref=dash" class="">Admin Settings</a></li>
             </ul>
-            <a href="../logout.php" class="logout-button button">Logout</a>
+            <a href="../logout.php" class="">Logout</a>
         </nav>
-        <main class="shark-page-body">
-            <form method="POST" class="shark-form">
-                
+        <main class="">
+            <form method="POST" class="">
+
             </form>
         </main>
     </body>
