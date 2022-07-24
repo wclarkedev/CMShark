14d03b70c9ba520327553fffb29fc897c6366754
Will Clarke <clarke19w@gmail.com>
2022-07-24 22:32:06 +0100

    start

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+//require './src/functions.php';
+?>
+<!doctype html><html lang="en">
+    <head>
+        <meta name="charset" content="utf-8">
+        <meta name="viewport" content="width=device-width,intitial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="./public/tailwind-config.js"></script>
+    </head>
+</html>
