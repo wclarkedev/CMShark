207727d3296c515d218ace26a3480a837292b3e8
Will Clarke <clarke19w@gmail.com>
2022-07-24 17:12:23 +0100

    form example done- hopefully it works

--- tests/ContentDelivery/FullFormExample.php
+++ tests/ContentDelivery/FullFormExample.php
@@ -1,3 +1,20 @@
+<?php
+require 'db.php';
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    $error = '';
+    if (empty(trim($_POST['title'])) && empty(trim($_POST['description']))&& empty(trim($_POST['href']))) $error = 'One or more fields are empty';
+    else {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $href = $_POST['href'];
+        $connection = new mysqli($host,$user,$password,$db);
+        $query = "INSERT INTO tests (linkTitle,linkDescription,link) VALUES (".$title.",".$description.",".$href.")";
+        if ($connection->query($query) == true) echo "Records created";
+        else echo "Record creation failed: ".$connection->error;
+        $connection->close();
+    }
+}
+?>
 <!DOCTYPE html><html lang="en">
     <head>
         <meta name="viewport" content="width=device-width,intial-scale=1.0">
@@ -18,17 +35,18 @@
     <body>
         <form method="POST">
             <h3>Create new link</h3>
+            <label class="php-error"><?php echo "error:". $error;?></label>
             <div>
-                <label></label>
-                <input type="text" name="">
+                <label>Title</label>
+                <input type="text" name="title">
             </div>
             <div>
-                <label></label>
-                <input type="text" name="">
+                <label>Description:</label>
+                <input type="text" name="description">
             </div>
             <div>
-                <label></label>
-                <input type="text" name="">
+                <label>Hyperlink: <code>including https://</code></label>
+                <input type="text" name="href">
             </div>
             <input type="submit">
         </form>
