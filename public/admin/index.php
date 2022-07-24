78fbcbc21bf9797172637dd09c8e1179205ceeb6
Will Clarke <clarke19w@gmail.com>
2022-07-24 20:30:08 +0100

    remove styling from old ui

--- public/admin/index.php
+++ public/admin/index.php
@@ -3,3 +3,9 @@ require '../../src/functions.php';
 //require '../../config/config.php';
 //redirectForNotLoggedIn();
 ?>
+<!doctype html><html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width,intial-scale=1.0">
+        <link href="../../src/css/framework.css" rel="stylesheet">
+    </head>
+</html>
