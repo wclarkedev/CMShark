c124b845a5b57a13481bba928cc690daca154f8d
Will Clarke <clarke19w@gmail.com>
2022-07-24 17:01:19 +0100

    more appropriate names + new file

---
+++ tests/ContentDelivery/FullFormExample.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html><html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width,intial-scale=1.0">
+    </head>
+    <style>
+        html {font-family:sans-serif;}
+        form {
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+        }
+        form div {
+            display:flex;
+            flex-direction:column;
+        }
+    </style>
+    <body>
+        <form method="POST">
+            <h3>Create new link</h3>
+            <div>
+                <label></label>
+                <input type="text" name="">
+            </div>
+            <div>
+                <label></label>
+                <input type="text" name="">
+            </div>
+            <div>
+                <label></label>
+                <input type="text" name="">
+            </div>
+            <input type="submit">
+        </form>
+    </body>
+</html>
