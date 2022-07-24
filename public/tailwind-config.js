dbd6ea0fba36ea0a56c2e3f872a0a8a93b29dc5a
Will Clarke <clarke19w@gmail.com>
2022-07-25 00:11:09 +0100

    finish link design

--- public/tailwind-config.js
+++ public/tailwind-config.js
@@ -4,6 +4,7 @@ tailwind.config = {
             colors:{
                 'backgroundColor':'#12141D',
                 'backgroundAccent':'#1E2029',
+                'backgroundAccentLighter':'#22242d',
                 'accent':'#DC3962',
                 'lightAccent':'#FF715B',
                 'primaryText':'#FFFFFF',
