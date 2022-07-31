91068fbb95e2fe3dd966c5fc22b35ca297771a2f
unknown <clarke19w@gmail.com>
2022-07-31 17:37:39 +0000

    web vitals optimisation + code readability

--- public/index.php
+++ public/index.php
@@ -4,11 +4,15 @@
         <meta name="viewport" content="width=device-width,intitial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="../public/tailwind.config.js"></script>
+        <title></title>
+        <meta name="title" content="">
+        <meta name="description" content="">
     </head>
-    <body id="page:landing" class="bg-backgroundColor">
+    <body class="bg-backgroundColor">
         <section id="header" class="w-4/12 bg-backgroundAccent flex flex-col mx-auto rounded-md mt-10">
             <div class="flex flex-col mx-auto my-6">
-                <img class="rounded-full h-32 w-32" src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80">
+                <img class="rounded-full h-32 w-32" alt="Profile Picture" 
+                src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80">
             </div>
             <div class="flex flex-col mx-auto mb-6">
                 <h1 class="text-primaryText text-3xl font-bold text-center mb-2">Placeholder Name</h1>
@@ -16,16 +20,21 @@
             </div>
         </section>
         <section id="links" class="w-4/12 flex flex-col mx-auto mb-10 mt-1">
-            <a href="" id="link" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
-                <div class="py-2 px-2"><img src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80" class="rounded-sm h-20 w-20"></div>
+            <a href="" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
+                <div class="py-2 px-2">
+                    <img class="rounded-sm h-20 w-20" alt="placeholder Logo"
+                    src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80">
+                </div>
                 <div class="flex flex-col py-4 px-6">
                     <h3 class="text-xl text-primaryText">Placeholder Title</h3>
                     <span class="text-base text-secondaryText pt-1">Placeholder Description</span>
                 </div>
             </a>
-            <a href="" id="link" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
-                <div class="py-2 px-2"><img src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80" class="rounded-sm h-20 w-20"></div>
-                <div class="flex flex-col py-4 px-6">
+            <a href="" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
+                    <div class="py-2 px-2">
+                        <img class="rounded-sm h-20 w-20" alt="placeholder Logo"
+                        src="https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80">
+                    </div>                <div class="flex flex-col py-4 px-6">
                     <h3 class="text-xl text-primaryText">Placeholder Title</h3>
                     <span class="text-base text-secondaryText pt-1">Placeholder Description</span>
                 </div>
