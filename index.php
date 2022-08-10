f9ad877ce02531ee522d8c671f78ae53e0c64946
Will Clarke <clarke19w@gmail.com>
2022-08-10 20:33:10 +0100

    social icons

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta name="viewport" content="width=device-width,intitial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="tailwind.config.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title></title>
         <meta name="title" content="">
         <meta name="description" content="">
@@ -17,6 +18,27 @@
             <div class="flex flex-col mx-auto mb-6">
                 <h1 class="text-primaryText text-3xl font-bold text-center mb-2">Placeholder Name</h1>
                 <span class="text-base text-secondaryText text-center max-w-xs">Placeholder bio</span>
+            </div>
+            <div class="w-6/12 flex flex-wrap flex-row mx-auto mb-6 justify-center">
+                <!-- Icons for social media -->
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-github"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-bitbucket"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-gitlab"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-behance"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-linkedin"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-medium"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-patreon"></i></a>
+                <!--<a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-solid fa-envelope"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-solid fa-phone"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-discord"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-quora"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-reddit"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-twitter"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-twitch"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-instagram"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-facebook"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-discourse"></i></a>
+                <a class="text-xl text-primaryText py-1 px-2" href=""><i class="fa-brands fa-whatsapp"></i></a>-->
             </div>
         </section>
         <section id="links" class="w-4/12 flex flex-col mx-auto mb-10 mt-1">
