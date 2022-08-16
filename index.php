6128059e0d04d17ed9faf6ebec785eab7ef4f0a7
Will Clarke <clarke19w@gmail.com>
2022-08-16 19:13:55 +0100

    fix lighthouse seo score

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require './functions.php';
 <!doctype html><html lang="en">
     <head>
         <meta name="charset" content="utf-8">
-        <meta name="viewport" content="width=device-width,intitial-scale=1.0">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="tailwind.config.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" crossorigin="anonymous">
