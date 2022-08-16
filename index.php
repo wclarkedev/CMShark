e85b7d034c6b6b338ddf56c39b1099d32ad7bcdd
Will Clarke <clarke19w@gmail.com>
2022-08-16 19:00:46 +0100

    get rid of random stuff on link

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require './functions.php';
         <meta name="viewport" content="width=device-width,intitial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="tailwind.config.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" crossorigin="anonymous">
         <title><?php echo getPageMeta('title');?></title>
         <meta name="title" content="<?php echo getPageMeta('title');?>">
         <meta name="description" content="<?php echo getPageMeta('description');?>">
