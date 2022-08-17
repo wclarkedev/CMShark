89f317cdee3f52a160fa6c0509e8dcb523f891b9
Will Clarke <clarke19w@gmail.com>
2022-08-17 21:48:27 +0100

    social media section

--- admin/index.php
+++ admin/index.php
@@ -32,8 +32,8 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
                 <h1 class="text-primaryText text-3xl text-center font-semibold">Edit Page</h1>
                 <section class="flex flex-col mx-auto w-8/12" id="edit-page-form-container">
                     
-                    <form action="POST" class="flex flex-col mt-5">
-                        <div class="bg-backgroundAccent rounded w-6/12 flex flex-col mx-auto">
+                    <form action="POST" class="flex flex-col mt-2">
+                        <div class="bg-backgroundAccent rounded w-6/12 flex flex-col mx-auto py-2 my-3">
                             <h2 class="text-2xl text-primaryText place-self-center my-2">Page Header</h2>
                             <div class="flex flex-col mx-5 mb-5 place-self-center w-6/12">
                                 <label class="text-primaryText text-lg mb-1">Name</label>
@@ -51,6 +51,11 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
                                     <input type="file" class="hidden" value="Upload-Image" name="userProfilePicture"> 
                                 </label>
                             </div>
+
+                        </div>
+                        <div class="bg-backgroundAccent rounded w-6/12 flex flex-col mx-auto py-2 my-3">
+                            <h2 class="text-2xl text-primaryText place-self-center my-2">Social Media</h2>
+                            <div class="flex flex-col mx-5 mb-5 place-self-center w-6/12"></div>
                         </div>
                     </form>
                 </section>
