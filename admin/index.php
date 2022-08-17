08d64b3d299081b5b06a65bd8c6756e102896d9d
Will Clarke <clarke19w@gmail.com>
2022-08-17 20:53:30 +0100

    map out pages

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,intial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
-        <script src="../tailwind.config.js"></script>
+        <script src="/tailwind.config.js"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
         <title>Admin</title>
         <meta name="title" content="Admin">
@@ -37,7 +37,11 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 
     // Admin settings page
     $router->get('/settings',function () {
-
+        ?>
+        <div class="mt-12">
+            <h1 class="text-primaryText text-3xl text-center">Settings</h1>
+        </div>
+    <?php
     });
 
     $router->run();
