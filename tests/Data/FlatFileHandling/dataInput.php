f8a04665450b01bb2e3474fee84ff155675be711
Will Clarke <clarke19w@gmail.com>
2022-08-19 17:54:01 +0100

    basic routes

--- tests/Data/FlatFileHandling/dataInput.php
+++ tests/Data/FlatFileHandling/dataInput.php
@@ -1,2 +1,19 @@
-<?php 
+<?php
 // data gathered and put into data.yml
+require '/vendor/autoload.php';
+$router = new \Bramus\Router\Router();
+$router->set404(function () {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    echo '404, route not found!';
+});
+$router->get('/',function (){
+    echo "hello world";
+});
+// Login form test
+$router->get('/form/login',function (){
+
+});
+// Different data types tests
+$router->get('/form/different_datatypes',function (){
+
+});
