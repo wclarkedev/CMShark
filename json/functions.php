6a2295aa4f9c8c314055aa8b00f31f61dd7b8a54
Will Clarke <clarke19w@gmail.com>
2022-08-21 20:54:14 +0100

    page content function

--- json/functions.php
+++ json/functions.php
@@ -1,18 +1,23 @@
 <?php
 function getPageContent ($contentType) {
     if (!isset($contentType)) exit();
+    $data = json_decode(file_get_contents('page.json'));
     switch ($contentType) {
         case 'page_header':
-            // return array with all page header data
+            $header_data = array();
+            $header_data['name'] = $data->{'name'};
+            $header_data['bio'] = $data->{'bio'};
+            $header_data['location'] = $data->{'location'};
+            return $header_data;
         break;
         case 'name':
-            // return page header name
+            return $data->{'name'};
         break;
         case 'bio':
-            // return page header bio
+            return $data->{'bio'};
         break;
         case 'location':
-            // return page header location
+            return $data->{'location'};
         break;
         case 'links':
             // return array of all links
@@ -20,3 +25,6 @@ function getPageContent ($contentType) {
     }
 }
 function socialIcons ($icon) {}
+$header_data = getPageContent('page_header');
+//var_dump($header_data);
+echo $header_data['name'];
