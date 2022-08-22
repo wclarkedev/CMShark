68a87a82b56297d3688e81fa9fe96df5a9917d6a
Will Clarke <clarke19w@gmail.com>
2022-08-22 18:38:39 +0100

    setup routes

--- setup/index.php
+++ setup/index.php
@@ -1,4 +1,22 @@
 <?php
-// setup/?page=
+$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
 
-session_start();
+require '../vendor/autoload.php';
+$router = new \Bramus\Router\Router();
+$router->set404(function () {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    echo '404, route not found!';
+});
+$router->get('/', function () {
+
+});
+$router->get('/repair', function () {
+
+});
+$router->get('/repair/recreate', function () {
+
+});
+$router->run();
