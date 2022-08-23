d0a824adb4da1b52354fa869dd3f3f71ed1dd279
Will Clarke <clarke19w@gmail.com>
2022-08-23 20:22:01 +0100

    add meta

--- setup/index.php
+++ setup/index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html><html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,intial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="/tailwind.config.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
+        <title>Setup CMShark</title>
+        <meta name="title" content="Setup CMShark">
+    </head>
+    <body class="bg-backgroundColor">
 <?php
 $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
@@ -29,3 +40,6 @@ $router->get('/repair/recreate', function () {
 
 });
 $router->run();
+?>
+    </body>
+</html>
