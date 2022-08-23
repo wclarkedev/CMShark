381b6bfca86843980a510fc4751428d1e767489b
Will Clarke <clarke19w@gmail.com>
2022-08-23 20:10:16 +0100

    spacing

--- index.php
+++ index.php
@@ -177,7 +177,7 @@
                             <a href="<?php echo $defaultLinks[$i]['href']?>" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
                                 <div class="py-2 px-2">
                                     <img class="rounded-sm h-20 w-20" alt="Link Logo" src="
-                                    <?php echo getImages('link_icon', $defaultLinks[$i]['href']);?>
+                                        <?php echo getImages('link_icon', $defaultLinks[$i]['href']);?>
                                     ">
                                 </div>                
                                 <div class="flex flex-col py-4 px-6">
@@ -195,7 +195,7 @@
                         <a href="<?php echo $links[$i]['link'];?>" class="bg-backgroundAccent my-1 h-24 text-primaryText flex-row flex rounded-md hover:bg-backgroundAccentLighter">
                             <div class="py-2 px-2">
                                 <img class="rounded-sm h-20 w-20" alt="Link Logo" src="
-
+                                    <?php echo getImages('link_icon', $links[$i]['link']);?>
                                 ">
                             </div>                
                             <div class="flex flex-col py-4 px-6">
