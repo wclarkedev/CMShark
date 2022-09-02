fb2d162a91f7d63db7d2a026bf83f36c7c22391c
Will Clarke <clarke19w@gmail.com>
2022-09-02 21:07:00 +0100

    add post routes

---
+++ setup/index.php
@@ -0,0 +1,52 @@
+<?php
+$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+    if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+        return false;
+    }
+
+    require '../vendor/autoload.php';
+
+    //? Create a Router
+    $router = new \Bramus\Router\Router();
+    $router->set404(function () {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+        echo '404, route not found!';
+    });
+?>
+<!DOCTYPE html><html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,intial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="/tailwind.config.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
+        <title>Admin | CMShark</title>
+        <meta name="title" content="Admin">
+    </head>
+    <body class="bg-backgroundColor">
+<?php
+    $router->get('/',function () {
+    });
+
+    $router->get('/account',function () {
+    });
+
+    $router->get('/page',function () {
+    });
+
+    $router->post('/',function () {
+
+    });
+
+    $router->post('/account',function () {
+
+    });
+
+    $router->post('/page',function () {
+
+    });
+
+    $router->run();
+?>
+    </body>
+</html>
