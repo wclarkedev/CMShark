c8740b1fd87a38adc165ae432f1b531d00242ee3
Will Clarke <clarke19w@gmail.com>
2022-09-03 23:51:47 +0100

    remove unused file

--- blade-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('vendor/autoload.php');
-use eftec\bladeone\BladeOne;
-$views = __DIR__ . '/views';
-$cache = __DIR__ . '/cache';
-$blade = new BladeOne ( $views , $cache , BladeOne::MODE_AUTO); 
-
-echo $blade->run("boxed");
