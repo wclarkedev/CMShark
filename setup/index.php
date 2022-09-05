5a29f9f7df879307a0a4efe1726fa2d7e2bdfa28
Will Clarke <clarke19w@gmail.com>
2022-09-05 19:12:43 +0100

    welcome page styling

--- setup/index.php
+++ setup/index.php
@@ -29,12 +29,12 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
         ?>
         <div class="flex flex-col mx-auto">
             <div class="flex flex-col mx-auto w-5/12 mt-12" id="welcome-container">
-                <h1 class="text-primaryText text-center text-3xl font-semibold" >Welcome to CMShark</h1>
-                <span class="text-primaryText my-5">
+                <h1 class="text-primaryText text-center text-4xl font-semibold" >Welcome to CMShark</h1>
+                <span class="text-primaryText my-5 text-lg">
                     CMShark is an open source, customisable and, whitelabel link list experience. We have put together a series of setup steps 
                     to help you get started easily without needing to edit configuration files. 
                 </span>
-                <a href="account/" class="">Get Started</a>
+                <a class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold" href="account/">Get Started</a>
             </div>
         </div>
         <?php
@@ -43,30 +43,30 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
         // https://www.okta.com/blog/2021/03/security-questions/
         ?>
         <div class="flex flex-col mx-auto w-5/12 mt-12">
-            <h2 class="text-primaryText text-3xl text-center font-semibold">Account Setup</h2>
+            <h2 class="text-primaryText text-4xl text-center font-semibold">Account Setup</h2>
             <form class="" method="POST">
                 <section class="flex flex-col">
-                    <label>Username</label>
+                    <label class="text-lg text-primaryText">Username</label>
                     <input class="" type="text" placeholder="" name="account-setup-username">
                 </section>
                 <section class="flex flex-col">
-                    <label>Email</label>
+                    <label class="text-lg text-primaryText">Email</label>
                     <input class="" type="text" placeholder="" name="account-setup-email">
                 </section>
                 <section class="flex flex-col">
-                    <label>Password</label>
+                    <label class="text-lg text-primaryText">Password</label>
                     <input class="" type="password" placeholder="" name="account-setup-password">
                 </section>
                 <section class="flex flex-col">
-                    <label>Confirm Password</label>
+                    <label class="text-lg text-primaryText">Confirm Password</label>
                     <input class="" type="password" placeholder="" name="account-setup-confirm-password">
                 </section>
                 <section class="flex flex-col">
-                    <label>Security Question 1</label>
+                    <label class="text-lg text-primaryText">Security Question 1</label>
                     
                 </section>
                 <section class="flex flex-col">
-                    <label>Security Question 2</label>
+                    <label class="text-lg text-primaryText">Security Question 2</label>
                     
                 </section>
             </form>
