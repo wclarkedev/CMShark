a273d7ef2a84ea651d02ab1794dc18034ed05779
Will Clarke <clarke19w@gmail.com>
2022-09-07 17:48:39 +0100

    finish site setup page

--- setup/index.php
+++ setup/index.php
@@ -48,20 +48,20 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
             <small class="text-primaryText text-center my-2">Required fields <b class="text-red-600">*</b></small>
             <form class="" method="POST">
                 <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Username</label>
+                    <label class="text-lg text-primaryText my-2">Username <b class="text-red-600">*</b></label>
                     <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="e.g. jonny" name="account-setup-username">
                 </section>
                 <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-1">Email</label>
+                    <label class="text-lg text-primaryText my-1">Email <b class="text-red-600">*</b></label>
                     <small class="italic text-accent mb-2">* This must be a valid email that can recieve mail.</small>
                     <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="e.g. jonhstevens@cmshark.com" name="account-setup-email">
                 </section>
                 <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Password</label>
+                    <label class="text-lg text-primaryText my-2">Password <b class="text-red-600">*</b></label>
                     <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" placeholder="**************" name="account-setup-password">
                 </section>
                 <section class="flex flex-col w-6/12 mx-auto mt-4 mb-6">
-                    <label class="text-lg text-primaryText my-2">Confirm Password</label>
+                    <label class="text-lg text-primaryText my-2">Confirm Password <b class="text-red-600">*</b></label>
                     <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" placeholder="**************" name="account-setup-confirm-password">
                 </section>
                 <section class="flex flex-col w-6/12 mx-auto my-4">
@@ -88,17 +88,9 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
     $router->get('page/', function () {
         ?>
         <div class="flex flex-col mx-auto w-5/12 mt-12">
-            <h2 class="text-primaryText text-4xl text-center font-semibold">Account Setup</h2>
+            <h2 class="text-primaryText text-4xl text-center font-semibold">Site Setup</h2>
             <small class="text-primaryText text-center my-2">Required fields <b class="text-red-600">*</b></small>
             <form class="" method="POST">
-                <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Username</label>
-                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="e.g. jonny" name="account-setup-username">
-                </section>
-                <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Password</label>
-                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" placeholder="**************" name="account-setup-password">
-                </section>
                 <section class="flex flex-col w-6/12 mx-auto mt-4 mb-6">
                     <label class="text-lg text-primaryText my-2">Your CMShark API key <b class="text-red-600">*</b></label>
                     <small class="italic text-accent mb-3">* Don't have one? Click <a href="" class="underline hover:no-underline font-semibold">here</a> to get yours.</small>
@@ -110,7 +102,7 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
                     <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="" name="account-setup-confirm-password">
                 </section>
                 <section class="flex flex-col w-6/12 mx-auto mt-6 mb-4">
-                    <input class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold cursor-pointer" type="submit" value="Save & Continue">
+                    <input class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold cursor-pointer" type="submit" value="Save">
                 </section>
             </form>
         </div>
