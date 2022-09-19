8e64c9fa3b9cc7bf8de682a25b28f66ced9cc8fe
Will Clarke <clarke19w@gmail.com>
2022-09-19 17:04:50 +0100

    test 2fa

---
+++ tests/2fa/index.php
@@ -0,0 +1,12 @@
+<?php
+require '../../vendor/autoload.php';
+use PragmaRX\Google2FA\Google2FA;
+#use PragmaRX\Google2FAQRCode\Google2FA;
+
+$g = new Google2FA();
+
+
+$email = 'will@wclarke.dev';
+$name = 'cmshark';
+$secret_key = $g->generateSecretKey();
+
