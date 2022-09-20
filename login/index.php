72e21717560664254c64eb3598f2927dc6db898b
Will Clarke <clarke19w@gmail.com>
2022-09-20 21:59:17 +0100

    new password form

--- login/index.php
+++ login/index.php
@@ -173,6 +173,22 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 
     $router->match('GET|POST', 'forgot-password/new-password/', function () {
         // TODO - Create form for new password 
+        ?>
+        <div class="flex flex-col mx-auto w-5/12 mt-12">
+            <h2 class="text-primaryText text-4xl text-center font-semibold">Password reset</h2>
+            <small class="text-primaryText text-center my-2">Set a new password</small>
+            <form class="" method="POST">
+                <section class="flex flex-col w-6/12 mx-auto my-4">
+                    <label class="text-lg text-primaryText my-2">Password</label>
+                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" name="reset-password-username-or-pw" autocomplete="off">
+                </section>
+                <section class="flex flex-col w-6/12 mx-auto mt-6 mb-4">
+                    <input class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold cursor-pointer" type="submit" value="Save">
+                    <span class=" text-center text-accent hover:underline my-10"><a href="/login"><i class="fa-solid fa-arrow-left"></i> Return to login</a></span> 
+                </section>
+            </form>
+        </div>
+        <?php
     });
     $router->run();
 ?>
