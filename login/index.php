53a10312a551028d037c7b5b2494286366c227b3
Will Clarke <clarke19w@gmail.com>
2022-09-20 22:13:55 +0100

    reconfigure error delivery

--- login/index.php
+++ login/index.php
@@ -33,35 +33,15 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
         if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) header('/admin');
         $oskk = json_decode(file_get_contents('../json/config.json'));
         $oskk = $oskk->{'key'};
-        ?>
-        <div class="flex flex-col mx-auto w-5/12 mt-12">
-            <h2 class="text-primaryText text-4xl text-center font-semibold">Login</h2>
-            <small class="text-primaryText text-center my-2">Required fields <b class="text-red-600">*</b></small>
-            <form class="" method="POST">
-                <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Username <b class="text-red-600">*</b></label>
-                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="e.g. jonny" name="login-username">
-                </section>
-                <section class="flex flex-col w-6/12 mx-auto my-4">
-                    <label class="text-lg text-primaryText my-2">Password <b class="text-red-600">*</b></label>
-                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" placeholder="**************" name="login-password">
-                </section>
-                <section class="flex flex-col w-6/12 mx-auto mt-6 mb-4">
-                    <input class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold cursor-pointer" type="submit" value="Login">
-                    <span class=" text-center text-accent hover:underline my-10"><a href="/"><i class="fa-solid fa-arrow-left"></i> Return to site</a></span> 
-                    <span class=" text-center text-accent hover:underline my-6"><a href="forgot-password/">Forgot password?</a></span> 
-                </section>
-            </form>
-        </div>
-        <?php
+        $error = '';
         if ($_SERVER['REQUEST_METHOD']=='POST') {
             if (empty(trim($_POST['login-username']))) {
-                echo '<span style="color:red;">One or more required fields are empty</span>';
+                $error = 'One or more required fields are empty';
                 exit();
             }
             $username = $_POST['login-username'];
             if (empty(trim($_POST['login-password']))) {
-                echo '<span style="color:red;">One or more required fields are empty</span>';
+                $error = 'One or more required fields are empty';
                 exit();
             }
             $password = $_POST['login-password'];
@@ -81,16 +61,40 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
             $depw = openssl_decrypt($epw, $cipher, $oskk, $options, $iv);
 
             if ($un != $username) {
-                echo '<span style="color:red;">Incorrect Credentials</span>';
+                $error = 'Incorrect Credentials';
                 exit();
             }
             if ($depw != $password) {
-                echo '<span style="color:red;">Incorrect Credentials</span>';
+                $error = 'Incorrect Credentials';
                 exit();
             }
             $_SESSION['loggedin'] = true;
             header("Location: /admin/");
         }
+        ?>
+        <div class="flex flex-col mx-auto w-5/12 mt-12">
+            <h2 class="text-primaryText text-4xl text-center font-semibold">Login</h2>
+            <small class="text-primaryText text-center my-2">Required fields <b class="text-red-600">*</b></small>
+            <div class="" id="error-container">
+                <p class="" id="error"><?= $error ?></p>
+            </div>
+            <form class="" method="POST">
+                <section class="flex flex-col w-6/12 mx-auto my-4">
+                    <label class="text-lg text-primaryText my-2">Username <b class="text-red-600">*</b></label>
+                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="text" placeholder="e.g. jonny" name="login-username">
+                </section>
+                <section class="flex flex-col w-6/12 mx-auto my-4">
+                    <label class="text-lg text-primaryText my-2">Password <b class="text-red-600">*</b></label>
+                    <input class="bg-backgroundAccent py-2 px-3 text-primaryText rounded-sm placeholder:text-secondaryText" type="password" placeholder="**************" name="login-password">
+                </section>
+                <section class="flex flex-col w-6/12 mx-auto mt-6 mb-4">
+                    <input class="text-center text-lg text-primaryText ring ring-accent bg-accent w-fit place-self-center py-1 px-3 rounded-sm hover:underline font-semibold cursor-pointer" type="submit" value="Login">
+                    <span class=" text-center text-accent hover:underline my-10"><a href="/"><i class="fa-solid fa-arrow-left"></i> Return to site</a></span> 
+                    <span class=" text-center text-accent hover:underline my-6"><a href="forgot-password/">Forgot password?</a></span> 
+                </section>
+            </form>
+        </div>
+        <?php
     });
     // TODO - Backend logic for pw recovery 
 
@@ -172,7 +176,6 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
     });
 
     $router->match('GET|POST', 'forgot-password/new-password/', function () {
-        // TODO - Create form for new password 
         ?>
         <div class="flex flex-col mx-auto w-5/12 mt-12">
             <h2 class="text-primaryText text-4xl text-center font-semibold">Password reset</h2>
@@ -190,6 +193,7 @@ $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
         </div>
         <?php
     });
+
     $router->run();
 ?>
 </body>
